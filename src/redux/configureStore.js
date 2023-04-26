f4f81431a8c2a17467a92de579886d82a5a34bb0
Michael Trevino <michaeltrevino@ph-1525-22.pharr-tx.local>
2023-04-26 16:37:04 -0500

    preparing for deployment

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { createWrapper } from "next-redux-wrapper";
 import employeeInfoReducer from "./ducks/employeeInfo";
 import menuInfoReducer from "./ducks/menu";
 import modalReducer from "./ducks/modals";
